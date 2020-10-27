ec69182ace86d628c850c2e8d797cd76ef9dfa86
ShawnDa <shawnda761@gmail.com>
2020-10-27 22:19:58 +1300

    cart component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import Cart from './components/Cart';
 import Filter from './components/Filter';
 import Products from './components/Products';
 import data from './data.json'
@@ -9,15 +10,20 @@ class App extends Component {
 
     this.state = {
       products: data.products,
+      cartItems: [],
       size: "",
       sort: "",
     }
   }
 
   filterProducts = (event) => {
+    const sortedProducts = data.products.slice().sort((a, b) => (
+      (this.state.sort === "lowest") ? ((a.price > b.price) ? 1 : -1) : (this.state.sort === "highest") ? ((a.price < b.price) ? 1 : -1) : ((a._id > b._id) ? 1 : -1)
+    ));
+
     this.setState({
       size: event.target.value,
-      products: (event.target.value === "") ? data.products : data.products.filter(product => product.avaliableSizes.includes(event.target.value))
+      products: (event.target.value === "") ? sortedProducts : sortedProducts.filter(product => product.avaliableSizes.includes(event.target.value))
     });
   }
 
@@ -28,6 +34,27 @@ class App extends Component {
         (event.target.value === "lowest") ? ((a.price > b.price) ? 1 : -1) : (event.target.value === "highest") ? ((a.price < b.price) ? 1 : -1) : ((a._id > b._id) ? 1 : -1)
       ))
     });
+  }
+
+  removeFromCart = (product) => {
+    this.setState({
+      cartItems: this.state.cartItems.slice().filter(item => item._id !== product._id)
+    })
+  }
+
+  addToCart = (product) => {
+    const cartItems = this.state.cartItems.slice();
+    let existFlag = false;
+    cartItems.forEach((item) => {
+      if (item._id === product._id) {
+        item.count++;
+        existFlag = true;
+      }
+    });
+    if (!existFlag) {
+      cartItems.push({...product, count: 1});
+    }
+    this.setState({ cartItems });
   }
 
   render() {
@@ -44,9 +71,11 @@ class App extends Component {
                 filterProducts={this.filterProducts} 
                 sortProducts={this.sortProducts} 
               />
-              <Products products={this.state.products} />
+              <Products products={this.state.products} addToCart={this.addToCart} />
             </div>
-            <div className="sidebar">Cart Items</div>
+            <div className="sidebar">
+              <Cart cartItems={this.state.cartItems} removeFromCart={this.removeFromCart} />
+            </div>
           </div>
         </main>
         <footer>All right is reserved.</footer>
