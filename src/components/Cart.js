1c0b5580a68d18829be6726b9a566719c32eb50a
ShawnDa <shawnda761@gmail.com>
2020-10-28 17:37:54 +1300

    checkout component

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,15 +1,42 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
 import formatCurrency from '../util';
 
 export default class Cart extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: "",
+      name: "",
+      address: "",
+      showCheckout: false
+    }
+  }
+
+  handleInput = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+
+  createOrder = (e) => {
+    e.preventDefault();
+
+    const order = {
+      email: this.state.email,
+      name: this.state.name,
+      address: this.state.address,
+      cartItems: this.props.cartItems
+    }
+    this.props.createOrder(order);
+  }
+
   render() {
-    const {cartItems} = this.props;
     return (
       <div>
-        {cartItems.length === 0 ? <div className="cart cart-header">Cart is empty</div> : <div className="cart cart-header">You have {cartItems.length} item{cartItems.length === 1 ? "" : "s"} in the cart{" "}</div>}
+        {this.props.cartItems.length === 0 ? <div className="cart cart-header">Cart is empty</div> : <div className="cart cart-header">You have {this.props.cartItems.length} item{this.props.cartItems.length === 1 ? "" : "s"} in the cart{" "}</div>}
         <div className="cart">
           <ul className="cart-items">
-            {cartItems.map(item => (
+            {this.props.cartItems.map(item => (
               <li key={item._id}>
                 <div>
                   <img src={item.image} alt={item.title} />
@@ -25,16 +52,41 @@ export default class Cart extends Component {
             ))}
           </ul>
         </div>
-        {cartItems.length !== 0 && ( 
-          <div className="cart">
-            <div className="total">
-              <div>
-                Total:{" "}
-                {formatCurrency(cartItems.reduce((a, i) => a + i.price * i.count, 0))}
+        {this.props.cartItems.length !== 0 && (
+          <Fragment>
+            <div className="cart">
+              <div className="total">
+                <div>
+                  Total:{" "}
+                  {formatCurrency(this.props.cartItems.reduce((a, i) => a + i.price * i.count, 0))}
+                </div>
+                <button className="button primary" onClick={() => { this.setState({ showCheckout: true }); }}>Proceed</button>
               </div>
-              <button className="button primary">Proceed</button>
             </div>
-          </div>
+            {this.state.showCheckout && (
+              <div className="cart">
+                <form onSubmit={ this.createOrder }>
+                  <ul className="form-container">
+                    <li>
+                      <label>Email</label>
+                      <input type="email" name="email" required onChange={this.handleInput} />
+                    </li>
+                    <li>
+                      <label>Name</label>
+                      <input type="text" name="name" required onChange={this.handleInput} />
+                    </li>
+                    <li>
+                      <label>Address</label>
+                      <input type="text" name="address" required onChange={this.handleInput} />
+                    </li>
+                    <li>
+                      <button type="submit" className="button primary">Checkout</button>
+                    </li>
+                  </ul>
+                </form>
+              </div>
+            )}
+          </Fragment>
         )}
       </div>
     )
--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,31 +1,29 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-export default class Filter extends Component {
-  render() {
-    return (
-      <div className="filter">
-        <div className="filter-result">{this.props.count} Products</div>
-        <div className="filter-sort">
-          Order{" "} 
-          <select value={this.props.sort} onChange={this.props.sortProducts}>
-            <option>Latest</option>
-            <option value="lowest">Lowest</option>
-            <option value="highest">Highest</option>
-          </select>
-        </div>
-        <div className="filter-size">
-          Filter{" "}
-          <select value={this.props.size} onChange={this.props.filterProducts}>
-            <option value="">ALL</option>
-            <option value="XS">XS</option>
-            <option value="S">S</option>
-            <option value="M">M</option>
-            <option value="L">L</option>
-            <option value="XL">XL</option>
-            <option value="XXL">XXL</option>
-          </select>
-        </div>
-      </div>
-    )
-  }
-}
+const Filter = ({ count, sort, sortProducts, size, filterProducts }) => (
+  <div className="filter">
+    <div className="filter-result">{count} Products</div>
+    <div className="filter-sort">
+      Order{" "} 
+      <select value={sort} onChange={sortProducts}>
+        <option>Latest</option>
+        <option value="lowest">Lowest</option>
+        <option value="highest">Highest</option>
+      </select>
+    </div>
+    <div className="filter-size">
+      Filter{" "}
+      <select value={size} onChange={filterProducts}>
+        <option value="">ALL</option>
+        <option value="XS">XS</option>
+        <option value="S">S</option>
+        <option value="M">M</option>
+        <option value="L">L</option>
+        <option value="XL">XL</option>
+        <option value="XXL">XXL</option>
+      </select>
+    </div>
+  </div>
+)
+
+export default Filter;
